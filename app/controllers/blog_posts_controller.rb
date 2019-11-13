70095df0b2a61d99cfcfe67f95e0b764518b9fbb
learn <learn@learn08.local>
2019-11-12 16:09:30 -0800

    complete rails blog challenge

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -8,4 +8,30 @@ class BlogPostsController < ApplicationController
     @post = BlogPost.find(params[:id])
     #render "show.html.erb"
   end
+
+  def new
+  end
+
+  def create
+      @post = BlogPost.create(
+          title: params[:title],
+          content: params[:content]
+      )
+
+      if @post.save
+          redirect_to @post
+      else
+          render action: :create
+      end
+  end
+
+    def delete
+        @post = BlogPost.find(params[:id])
+
+        if @post.destroy
+            redirect_to all_posts_path
+        else
+            redirect_to blog_post_path(@post)
+        end
+    end
 end
